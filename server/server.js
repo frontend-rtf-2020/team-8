d1c42225be311626792239b3993389881aad56fd
Alexander Popov <AlexPopov1999@gmail.com>
2020-03-24 15:56:35 +0500

    created express server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,9 @@
-import path from 'path'
-import express from 'express'
-import { MongoClient } from 'mongodb'
-import template from './../template'
-//comment out before building for production
-import devBundle from './devBundle'
+import config from './../config/config'
+import app from './express'
 
-const app = express()
-//comment out before building for production
-devBundle.compile(app)
-
-const CURRENT_WORKING_DIR = process.cwd()
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
-
-app.get('/', (req, res) => {
-  res.status(200).send(template())
-})
-
-let port = process.env.PORT || 3000
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', port)
+  console.info('Server started on port %s.', config.port)
 })
-
