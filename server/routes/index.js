50072d81d9d3a56e9c43dce624359f3e87f55210
Alexander Popov <AlexPopov1999@gmail.com>
2020-04-07 23:32:12 +0500

    change package sctipts for nodemon

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('API Running');
 });
 
 module.exports = router;
