8ba9054daf79f5d7d761a0bf4b75f60e5421a6ba
FeeLeD <feeled9@gmail.com>
2020-04-07 11:24:08 +0500

    concurrently-setup

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: 'mongodb+srv://team:QgOVTyn4wOcG8MbV@users-f5jfa.mongodb.net/test?retryWrites=true&w=majority'
   }
