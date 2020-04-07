d1933dbaff538bd0e3e8569e2a8f80a56f35a5f6
Alexander Popov <AlexPopov1999@gmail.com>
2020-04-07 22:32:33 +0500

    changed scripts into package.json

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import path from 'path'
 
 
 let app = express();
 
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 
 export default app;
