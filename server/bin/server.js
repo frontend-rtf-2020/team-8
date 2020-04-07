69b3c0c568317d3eae0bdc5b8766032713950973
FeeLeD <feeled9@gmail.com>
2020-04-07 11:59:02 +0500

    skeleton-setup

--- server/bin/server.js
+++ server/bin/server.js
@@ -6,4 +6,5 @@ app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', config.port)})
+  console.info('Server started on port %s.', config.port)
+})
