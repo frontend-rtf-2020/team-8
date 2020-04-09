d1748b8bb5e528a57d318dae4368a7008f2d54a3
FeeLeD <feeled9@gmail.com>
2020-04-09 21:15:59 +0500

    jwt-get

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import auth from '../../middleware/auth';
+import User from '../../models/User';
 
 const router = express.Router();
 
 // @route       GET api/auth
 // @access      Public
-router.get('/', (req, res) => res.send('Auth router'));
+router.get('/', auth, async (req, res) =>{
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch(err) {
+        res.status(500).send('Server Error');
+    }
+});
 
 export default router;
