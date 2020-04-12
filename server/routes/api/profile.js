90aa8fca7fdbd6eab0c6d2182ffbd7007eeac4ef
FeeLeD <feeled9@gmail.com>
2020-04-12 19:40:01 +0500

    delete-profile-user-api

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -119,4 +119,22 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+
+// @route       DELETE api/profile/
+// @desc        Delete profile and user
+// @access      Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id }); 
+        // Remove user       
+        await User.findOneAndRemove({ _id: req.user.id });      
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 export default router;
