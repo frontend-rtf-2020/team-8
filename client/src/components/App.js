b7febd9c2e79cc28187f1207f1c097c723a78879
FeeLeD <feeled9@gmail.com>
2020-04-19 12:56:33 +0500

    Router added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,20 @@
-import React, {Fragment} from 'react';
-import '../stylesheets/reset.css'
-import '../stylesheets/App.css';
-import Auth from '../components/Auth';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from './Auth';
+import Register from './Register';
+import Users from './Users';
 
-function App() {
-  return (
-    <Fragment>
-      <Auth></Auth>
-    </Fragment>
-  );
-}
+import '../stylesheets/reset.css';
+
+const App = () => (
+  <Router>
+    <Route exact path="/" component={Auth} />
+    <Switch>
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Auth} />
+      <Route exact path="/users" component={Users} />
+    </Switch>
+  </Router>
+);
 
 export default App;
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom';
 import '../stylesheets/auth.css'
 
-export default class Auth extends Component {
-    render() {
-        return (
-            <div className="Auth">
-                <div>
-                    <h1>Войдите в аккаунт</h1>
-                    <h2>Пожалуйста, войдите в свой аккаунт </h2>
-                    <form>
-                        <input type="email" placeholder="Ваш email" required="required" autocomplete="off"></input>
-                        <span className="left">Неверный адрес электронной почты</span>
-                        <input type="password" placeholder="Ваш пароль" required="required" autocomplete="off"></input>
-                        <span className="left">Неверный пароль</span>
-                        <span className="right">Забыли пароль?</span>
-                        <input type="submit" value="ВОЙТИ В АККАУНТ"></input>
-                        <span className="center">Зарегистрироваться</span>
-                    </form>
-                    <p>Copyright © 2020  Dream team Group RI-370005. All rights reserved.</p>
-                </div>
-            </div>
-        )
-    }
-}
+const Auth = () => (
+    <div className="Auth">
+        <div>
+            <h1>Войдите в аккаунт</h1>
+            <h2>Пожалуйста, войдите в свой аккаунт </h2>
+            <form>
+                <input type="email" placeholder="Ваш email" required="required" autocomplete="off"></input>
+                <span className="left">Неверный адрес электронной почты</span>
+                <input type="password" placeholder="Ваш пароль" required="required" autocomplete="off"></input>
+                <span className="left">Неверный пароль</span>
+                <span className="right">Забыли пароль?</span>
+                <input type="submit" value="ВОЙТИ В АККАУНТ"></input>
+                <Link to="/register">
+                    <span className="center">Зарегистрироваться</span>
+                </Link>
+                <Link to="/users">
+                    <span className="center">Пользователи</span>
+                </Link>
+            </form>
+            <p>Copyright © 2020  Dream team Group RI-370005. All rights reserved.</p>
+        </div>
+    </div>
+);
+
+export default Auth;
