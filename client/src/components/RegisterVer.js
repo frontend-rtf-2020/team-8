4646713badc1372f6b399657e155e7be0272a247
Daniil <p.danya@mail.ru>
2020-05-02 13:36:17 +0300

    Adapting and adding new forms (Registration)

---
+++ client/src/components/RegisterVer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../stylesheets/registerVer.css'
+
+const RegisterVer = () => (
+    <div className="RegVer">
+        <div>
+            <h1>Регистрация</h1>
+            <h2>Подтверждение аккаунта</h2>
+            <form>
+                <img src="../img/nailVer.png" alt="Письмо отправлено на почту"></img>
+                <span className="center">Подтвердите свой аккаунт</span>
+                <span className="center">На вашу почту отправлено письмо с ссылкой на подтверждение аккаунта.</span>
+            </form>
+            <p>Copyright © 2020  Dream team Group RI-370005. All rights reserved.</p>
+        </div>
+    </div>
+);
+
+export default RegisterVer;
