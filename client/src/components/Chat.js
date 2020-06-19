f0976fa7653f67768e3f8070a62091178be55d2c
Daniil <p.danya@mail.ru>
2020-06-19 15:29:26 +0500

    Обнолвление чата

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -17,11 +17,10 @@ const Chat = () => (
             </div>
             <div className="dialogs">
                 <div className="dialog">
-                    <div className="avatar">
-                    </div>
+                    <div className="avatar"/>
                     <div className="infoDialog">
                         <div className="topInfo">
-                            <span className="name">Daniil Popov</span>
+                            <span className="name">Anton Pipenko</span>
                             <span className="info">today</span>
                         </div>
                         <div className="bottomInfo">
@@ -34,12 +33,33 @@ const Chat = () => (
         </div>
         <div className="right">
             <div className="status">
-                <div>name</div>
-                <div>status</div>
+                <span className="name">Michel Lalala</span>
             </div>
-            <div className="">
-                Низ
+            <div className="main">
+                <div className="messages">
+                    <div className="incomingLetterWrapper">
+                        <div className="avatar"/>
+                        <div className="inLetter">
+                            <span>Apple - это лучшее что придумал человек!</span>
+                        </div>
+                        {/* <span className="info">Вчера, в 14:48</span> */}
+                    </div>
+                    <div className="outcomingLetterWrapper">
+                        <div className="inLetter">
+                            <span>На самом то деле, Сяоми топ за свои деньги!</span>
+                        </div>
+                        <div className="avatar"/>
+                    </div>
                 </div>
+                <div className="message">
+                    <div className="messageForm">
+                        <form action="" method="get">
+                            <input name="writeMessage" placeholder="White a message..." type="text"/>
+                            <button type="submit"></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
         {/* <p>Copyright © 2020  Dream team Group RI-370005. All rights reserved.</p> */}
     </div>
