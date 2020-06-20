7a3224378a12f42a8c688731633cc0ff6b457fbc
Alexander Popov <AD.Popov@icloud.com>
2020-06-20 17:46:50 +0500

    add log token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -59,6 +59,8 @@ const requireSignin = function (req, res, next) {
     // Get token from header
     const token = req.header('x-auth-token');
 
+    console.log(token);
+
     // Check if not token
     if (!token) {
         return res.status(401).json({ msg: "No token, authorization denied" });
